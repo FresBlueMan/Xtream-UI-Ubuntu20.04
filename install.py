df55335c3dddc660d46ea184203e53f25acf402f
FresBlueMan <56789260+FresBlueMan@users.noreply.github.com>
2025-04-24 19:34:24 +0800

    Update install.py

--- install.py
+++ install.py
@@ -51,10 +51,10 @@ def getVersion():
 def printc(rText, rColour=col.BRIGHT_GREEN, rPadding=0, rLimit=46):
     #print("%s ┌─────────────────────────────────────────────────┐ %s" % (rColour, col.ENDC))
     print("%s +-------------------------------------------------+ %s" % (rColour, col.ENDC))
-    for i in range(rPadding): print("%s l                                                 l %s" % (rColour, col.ENDC))
+    for i in range(rPadding): print("%s |                                                 | %s" % (rColour, col.ENDC))
     array = [rText[i:i+rLimit] for i in range(0, len(rText), rLimit)]
-    for i in array : print("%s l %s%s%s l %s" % (rColour, " "*round(23-(len(i)/2)), i, " "*round(46-(22-(len(i)/2))-len(i)), col.ENDC))
-    for i in range(rPadding): print("%s l                                                 l %s" % (rColour, col.ENDC))
+    for i in array : print("%s l %s%s%s | %s" % (rColour, " "*round(23-(len(i)/2)), i, " "*round(46-(22-(len(i)/2))-len(i)), col.ENDC))
+    for i in range(rPadding): print("%s |                                                 | %s" % (rColour, col.ENDC))
     #print("%s └─────────────────────────────────────────────────┘ %s" % (rColour, col.ENDC))
     print("%s +-------------------------------------------------+ %s" % (rColour, col.ENDC))
     print(" ")
